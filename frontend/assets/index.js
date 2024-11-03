44ead410bcb1fc012716a8c97ccf55011b193492
Hank Star <kate.stapley@hotmail.com>
2024-11-03 11:52:39 +1100

    form fixes

--- frontend/assets/index.js
+++ frontend/assets/index.js
@@ -2,7 +2,6 @@ document.getElementById("theme-toggle").addEventListener("click", function () {
     const body = document.body;
     body.classList.toggle("dark-mode");
   
-    // Toggle button text based on mode
     const isDarkMode = body.classList.contains("dark-mode");
     this.textContent = isDarkMode ? "Light Mode" : "Dark Mode";
   });
@@ -19,5 +18,15 @@ document.getElementById("theme-toggle").addEventListener("click", function () {
     document.getElementById("button-container").classList.add("hidden");
     document.getElementById("registration-form").classList.add("hidden");
     document.getElementById("login-form").classList.remove("hidden");
+  });
+  
+  document.getElementById("close-registration").addEventListener("click", function () {
+    document.getElementById("registration-form").classList.add("hidden");
+    document.getElementById("button-container").classList.remove("hidden");
+  });
+  
+  document.getElementById("close-login").addEventListener("click", function () {
+    document.getElementById("login-form").classList.add("hidden");
+    document.getElementById("button-container").classList.remove("hidden");
   });
   
