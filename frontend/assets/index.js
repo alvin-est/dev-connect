33c9c829c396b6310cc688d5f4e2298c2e2d73a8
Hank Star <kate.stapley@hotmail.com>
2024-11-03 11:45:36 +1100

    sign up and login mock ups added

--- frontend/assets/index.js
+++ frontend/assets/index.js
@@ -4,6 +4,20 @@ document.getElementById("theme-toggle").addEventListener("click", function () {
   
     // Toggle button text based on mode
     const isDarkMode = body.classList.contains("dark-mode");
-    this.textContent = isDarkMode ? "Switch to Light Mode" : "Switch to Dark Mode";
+    this.textContent = isDarkMode ? "Light Mode" : "Dark Mode";
+  });
+  
+  document.getElementById("sign-up-button").addEventListener("click", function (e) {
+    e.preventDefault();
+    document.getElementById("button-container").classList.add("hidden");
+    document.getElementById("login-form").classList.add("hidden");
+    document.getElementById("registration-form").classList.remove("hidden");
+  });
+  
+  document.getElementById("login-button").addEventListener("click", function (e) {
+    e.preventDefault();
+    document.getElementById("button-container").classList.add("hidden");
+    document.getElementById("registration-form").classList.add("hidden");
+    document.getElementById("login-form").classList.remove("hidden");
   });
   
