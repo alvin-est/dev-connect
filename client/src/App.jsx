93feecd5835c64d97b1c6505e0601803ab30be85
Hank Star <kate.stapley@hotmail.com>
2024-11-09 17:32:46 +1100

    reverted changes because I cant get the routing right

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Register from './components/Register';
+import Login from './components/Login';
 
 function App() {
     return (
